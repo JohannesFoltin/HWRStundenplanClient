fb83aaba38df96b8b4c40814a93adfe60e670cfc
Johannes <Johannes.Foltin@gmx.de>
2022-11-01 15:20:37 +0100

    richtige Reihenfolge der Online/Offline Abfragen siwpe gesetik eingeführt kleines UI Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:convert';
 import 'dart:async';
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stundenplan/Struct/plan.dart';
 import 'package:stundenplan/Struct/vorlesung.dart';
 import 'package:intl/intl.dart';
@@ -23,15 +27,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'HWR Studenplan'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -45,22 +48,71 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     _selectedDate = DateTime.now();
-    futurePlan = fetchPlan();
+    futurePlan = _loadPlanFormOnline();
   }
 
-  Future<Plan> fetchPlan() async {
-    final response =
-        await http.get(Uri.parse('http://192.168.178.33:4545/plan'));
+  void _showSnackBar(String text) {
+    var snackBar =
+        SnackBar(duration: Duration(seconds: 1), content: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text),
+          ],
+        ));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      return Plan.fromJson(jsonDecode(response.body));
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
+  var key = "SAVE";
+
+  Future<Plan> _loadPlanFromLocal() async {
+    final prefs = await SharedPreferences.getInstance();
+    try {
+      _showSnackBar("Erfolgreich Daten lokal geladen");
+      return Plan.fromJson(jsonDecode(prefs.getString(key) ?? ""));
+    } catch (error) {
+      _showSnackBar("Keine Daten lokal vorhanden");
+      return Future<Plan>.error("Keine Daten lokal vorhanden");
     }
+  }
+
+  Future<Plan> _loadPlanFormOnline() async {
+    try {
+      final response =
+          await http.get(Uri.parse('http://192.168.178.33:4545/plan'));
+      if (response.statusCode == 200) {
+        _showSnackBar("Erfolgreich aus dem Internet geladen");
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setString(key, response.body);
+        return Plan.fromJson(jsonDecode(response.body));
+      } else {
+        return Future<Plan>.error("Server antwortet komisch");
+      }
+    } catch (error) {
+      _showSnackBar("Server nicht erreicht. Versuche es aus dem Speicher");
+      return _loadPlanFromLocal();   
+      }
+
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        currentDate: DateTime.now(),
+        helpText: "Wähle einen Tag",
+        initialDate: _selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _pullRefresh() async {
+    setState(() {
+      futurePlan = _loadPlanFormOnline();
+    });
   }
 
   @override
@@ -68,64 +120,85 @@ class _MyHomePageState extends State<MyHomePage> {
     initializeDateFormatting('de');
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-                onPressed: () => {
-                      setState(() {
-                        // requestUpdate();
-                        futurePlan = fetchPlan();
-                      })
-                    },
-                icon: Icon(Icons.refresh))
-          ],
+          centerTitle: true,
+          leading: TextButton(
+            onPressed: () {
+              setState(() {
+                _selectedDate = DateTime.now();
+              });
+            },
+            child: Text(
+              "Heute",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          title: TextButton(
+            child: Text(
+              DateFormat.EEEE('de').format(_selectedDate),
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            onPressed: () => _selectDate(context),
+          ),
         ),
         body: FutureBuilder<Plan>(
             future: futurePlan,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                print(snapshot.data!.Vorlesungen.length);
-                return Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              _selectedDate =
-                                  _selectedDate.subtract(Duration(days: 1));
-                              setState(() {});
-                            },
-                            icon: Icon(Icons.arrow_left)),
-                        TextButton(
-                            onPressed: () {},
-                            child:
-                                Text(DateFormat.EEEE('de').format(_selectedDate))),
-                        IconButton(
-                            onPressed: () {
-                              _selectedDate =
-                                  _selectedDate.add(Duration(days: 1));
-                              setState(() {});
-                            },
-                            icon: Icon(Icons.arrow_right))
-                      ],
-                    ),
-                    Expanded(
+                return RefreshIndicator(
+                  onRefresh: () => _pullRefresh(),
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: GestureDetector(
+                        onHorizontalDragEnd: (details) {
+                          int sensitivity = 0;
+                          // Swiping in right direction.
+                          if (details.primaryVelocity! > sensitivity) {
+                            _selectedDate =
+                                _selectedDate.subtract(Duration(days: 1));
+                            setState(() {});
+                          }
+
+                          // Swiping in left direction.
+                          if (details.primaryVelocity! < sensitivity) {
+                            _selectedDate =
+                                _selectedDate.add(Duration(days: 1));
+                            setState(() {});
+                          }
+                        },
                         child: ListView.builder(
-                      itemCount: snapshot.data!
-                          .getVorlesungformDate(_selectedDate)
-                          .length,
-                      itemBuilder: (context, index) {
-                        return veranstaltungsWidget(snapshot.data!
-                            .getVorlesungformDate(_selectedDate)[index]);
-                      },
-                    ))
-                  ],
+                          itemCount: snapshot.data!
+                              .getVorlesungformDate(_selectedDate)
+                              .length,
+                          itemBuilder: (context, index) {
+                            return veranstaltungsWidget(snapshot.data!
+                                .getVorlesungformDate(_selectedDate)[index]);
+                          },
+                        ),
+                      ))
+                    ],
+                  ),
                 );
               } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
+                return RefreshIndicator(
+                  child: SingleChildScrollView(
+                    physics: AlwaysScrollableScrollPhysics(),
+                    child: SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        child: Center(child: Text('${snapshot.error}'))),
+                  ),
+                  onRefresh: () => _pullRefresh(),
+                );
               }
-              return Center(child: const CircularProgressIndicator());
+              return RefreshIndicator(
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  child: SizedBox(
+                      height: MediaQuery.of(context).size.height,
+                      child: Center(child: const CircularProgressIndicator())),
+                ),
+                onRefresh: () => _pullRefresh(),
+              );
             }));
   }
 
