2d8283a9b630d10c82a3885c9a08df2561cee62f
Johannes <Johannes.Foltin@gmx.de>
2022-12-08 17:34:44 +0100

    import zu löschen vergessen

--- lib/dayview.dart
+++ lib/dayview.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:stundenplan/Struct/vorlesung.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 
 class DayView extends StatelessWidget {
   const DayView(
